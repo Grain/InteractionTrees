6f8dfe99119c4788c7059a2fb39f93953c68ccdb
Paul He <paulhe@seas.upenn.edu>
2019-01-16 14:30:07 -0500

    Add trace file

---
+++ examples/Trace.v
@@ -0,0 +1,71 @@
+From ITree Require Import
+     ITree.
+
+Inductive E (O I : Type): Type := | E_1 : O -> I -> E O I.
+
+(* Trace style, a la Compcert (but with nondeterminism) *)
+Section Trace.
+  Inductive Event: Type :=
+  | E1: nat -> nat -> Event.
+  Inductive World : Type
+    (* := W (f : nat -> option (nat * World)) *)
+  .
+  Inductive State : Type.
+
+  (* Prop to handle nondeterminism *)
+  (* TODO: Previously part of world defn? what is the world if we take these out? *)
+  Inductive handler (e : Event) (w w' : World) : Prop.
+  Fixpoint handler_det (e : Event) (w : World) : World. Admitted.
+
+  (* handlers internal in operational semantics *)
+  Inductive big (w w' : World * State) : Prop.
+  Inductive small (w w' : World * State) : Prop.
+
+  (* handlers external to operational semantics *)
+  (* can use stream instead of list to model divergence *)
+  Inductive big' (s : State) (s' : State * list Event) : Prop.
+  Inductive small' (s : State) (s' : State * option Event) : Prop.
+
+  (* We can use the handler + the external semantics to simulate the internal semantics *)
+  Inductive big'' : (World * State) -> (World * State) -> Prop :=
+  | b_nil : forall w s s', big' s (s', nil) ->
+                      big'' (w, s) (w, s')
+  | b_cons : forall w w' w'' s s' s'' e l, big' s (s', (cons e l)) ->
+                                      handler e w w' ->
+                                      (* TODO: a different list of events can be used here, is that OK? *)
+                                      big'' (w', s') (w'', s'') ->
+                                      big'' (w, s) (w'', s'').
+
+  Inductive small'' : (World * State) -> (World * State) -> Prop :=
+  | s_none : forall w s s', small' s (s', None) ->
+                       small'' (w, s) (w, s')
+  | s_some : forall w w' s s' e, small' s (s', Some e) ->
+                            handler e w w' ->
+                            small'' (w, s) (w', s').
+
+End Trace.
+
+(* CoInductive World : Type. *)
+
+CoInductive World : Type :=
+  handler (f : forall O I, E O I -> O -> I * World).
+
+Inductive State : Type.
+
+(* "internal" handlers *)
+Definition big (w w' : World * State) : Prop := True.
+Definition small (w w' : World * State) : Prop := True.
+
+(* "external" handlers *)
+Definition big' {O I} (s : State) (s' : State * list (E O I)) :=
+  True.
+Definition small' {O I} (s : State) (s' : State * option (E O I)) :=
+  True.
+
+(* (* alternative definitions of world? *) *)
+
+(* Definition handler' W := forall O I, E O I -> O -> (I * W). *)
+(* Fail CoInductive world' := handler' world'. *)
+
+(* Definition handler'' W (X : W) := forall O I, E O I -> O -> (I * W). *)
+(* Definition world'' := exists W w, handler'' W w. *)
